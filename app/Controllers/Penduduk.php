1dcff681dec7501496504545f9aa55326f98a304
FATHUR RAHMAN ABDULLAH <63894938+fathurrahman0530@users.noreply.github.com>
2022-01-03 02:35:27 +0800

    update progress study

--- app/Controllers/Penduduk.php
+++ app/Controllers/Penduduk.php
@@ -13,7 +13,6 @@ class Penduduk extends BaseController
     {
 
         $this->pendudukModel = new PendudukModel();
-
     }
 
     public function index()
@@ -27,7 +26,6 @@ class Penduduk extends BaseController
         ];
 
         return view('penduduk/index', $data);
-
     }
 
     public function detail($slug)
@@ -41,32 +39,58 @@ class Penduduk extends BaseController
         ];
 
         if (empty($data['penduduk'])) {
-            
-            throw new \CodeIgniter\Exceptions\PageNotFoundException('Nik penduduk ' . $slug . ' tidak ditemukan..!!');
 
+            throw new \CodeIgniter\Exceptions\PageNotFoundException('Nik penduduk ' . $slug . ' tidak ditemukan..!!');
         }
 
         return view('penduduk/detail', $data);
-
     }
 
-    public function create(){
-        
+    public function create()
+    {
+
+        session();
         $data   = [
 
             'title' => 'Tambah Data Penduduk',
+            'validation' => \Config\Services::validation()
 
         ];
 
         return view('penduduk/create', $data);
-
     }
 
-    public function save(){
-        
+    public function save()
+    {
+        // validasi input
+        if (!$this->validate([
+            'nik' => [
+                'rules' => 'required|is_unique[tb_penduduk.nik]',
+                'errors'=> [
+                    'required'  => 'NIK harus diisi...!!!',
+                    'is_unique' => 'NIK sudah terdaftar...!!'
+                ],
+            ],
+            'namaPenduduk' =>[
+                'rules' => 'required',
+                'errors'=> [
+                    'required' => 'Nama penduduk harus diisi...!!!'
+                ],
+            ],
+            'alamat' => [
+                'rules' => 'required',
+                'errors'=> [
+                    'required' => '{field} harus diisi'
+                ],
+            ]
+        ])) {
+            $validation = \Config\Services::validation();
+            return redirect()->to('/penduduk/create')->withInput()->with('validation', $validation);
+        }
+
         // $slug = url_title($this->request->getVar('nik'), '-', true);
 
-       $this->pendudukModel->save([
+        $this->pendudukModel->save([
 
             'nik' => $this->request->getVar('nik'),
             'nama_penduduk' => $this->request->getVar('namaPenduduk'),
@@ -76,12 +100,10 @@ class Penduduk extends BaseController
             'image' => $this->request->getVar('gambar'),
             'slug' => $this->request->getVar('nik')
 
-       ]);
+        ]);
 
-       session()->setFlashdata('pesan', 'Data penduduk berhasil ditambahkan');
+        session()->setFlashdata('pesan', 'Data penduduk berhasil ditambahkan');
 
-       return redirect()->to('/penduduk');
-
+        return redirect()->to('/penduduk');
     }
-
 }
--- app/Views/penduduk/create.php
+++ app/Views/penduduk/create.php
@@ -10,37 +10,43 @@
         <div class="row mb-3">
           <label for="nik" class="col-sm-2 col-form-label">NIK</label>
           <div class="col-sm-10">
-            <input type="text" name="nik" class="form-control" id="nik" autofocus>
+            <input type="text" name="nik" class="form-control <?= ($validation->hasError('nik')) ? 'is-invalid' : ''; ?>" id="nik" autofocus value="<?= old('nik'); ?>">
+            <div id="nik" class="invalid-feedback">
+              <?= $validation->getError('nik'); ?>
+            </div>
           </div>
         </div>
         <div class="row mb-3">
           <label for="namaPenduduk" class="col-sm-2 col-form-label">Nama Penduduk</label>
           <div class="col-sm-10">
-            <input type="text" name="namaPenduduk" class="form-control" id="namaPenduduk">
+            <input type="text" name="namaPenduduk" class="form-control <?= ($validation->hasError('namaPenduduk')) ? 'is-invalid' : ''; ?>" id="namaPenduduk" value="<?= old('namaPenduduk'); ?>">
+            <div id="namaPenduduk" class="invalid-feedback">
+              <?= $validation->getError('namaPenduduk'); ?>
+            </div>
           </div>
         </div>
         <div class="row mb-3">
           <label for="jenisKelamin" class="col-sm-2 col-form-label">Jenis Kelamin</label>
           <div class="col-sm-10">
-            <input type="text" name="jenisKelamin" class="form-control" id="jenisKelamin">
+            <input type="text" name="jenisKelamin" class="form-control" id="jenisKelamin" value="<?= old('jenisKelamin'); ?>">
           </div>
         </div>
         <div class="row mb-3">
           <label for="agama" class="col-sm-2 col-form-label">Agama</label>
           <div class="col-sm-10">
-            <input type="text" name="agama" class="form-control" id="agama">
+            <input type="text" name="agama" class="form-control" id="agama" value="<?= old('agama'); ?>">
           </div>
         </div>
         <div class="row mb-3">
           <label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
           <div class="col-sm-10">
-            <input type="text" name="alamat" class="form-control" id="alamat">
+            <input type="text" name="alamat" class="form-control" id="alamat" value="<?= old('alamat'); ?>">
           </div>
         </div>
         <div class="row mb-3">
           <label for="gambar" class="col-sm-2 col-form-label">Gambar</label>
           <div class="col-sm-10">
-            <input type="text" name="gambar" class="form-control" id="gambar">
+            <input type="text" name="gambar" class="form-control" id="gambar" value="<?= old('gambar'); ?>">
           </div>
         </div>
         <button type="submit" name="save" class="btn btn-primary">Save</button>
@@ -48,4 +54,4 @@
     </div>
   </div>
 </div>
-<?php $this->endSection();
+<?php $this->endSection(); ?>
